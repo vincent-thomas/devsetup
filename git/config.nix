692359a50950cc6f58f58f5c3febacd73c9cb4a6
Vincent Thomas <vincent@v-thomas.com>
2026-06-22 00:07:53 +0200

    add 'git sameas' alias

--- git/config.nix
+++ git/config.nix
@@ -12,6 +12,7 @@
     l = log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(r) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative
     d = diff --word-diff --word-diff-regex='\\w+|[^[:space:]]'
     b = !git checkout $(git branch | tr -d ' *' | fzf)
+    sameas = !f() { a=$(git rev-parse HEAD 2>/dev/null) && b=$(git rev-parse "$@" 2>/dev/null) && [ "$a" = "$b" ] && echo true || { echo false; false; }; }; f
 
   [commit]
     gpgsign = true
