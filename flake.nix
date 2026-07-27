f9ee52c2633a429b940a6392593d022e1353ee0d
Vincent Thomas <vincent@v-thomas.com>
2026-07-27 21:31:26 +0200

    Remove the obsolete Fenix dependency
    What: Drop Fenix and its rust-analyzer source from the declared and locked flake graph.
    Why: Rustup is now the sole Rust distribution mechanism, so retaining Fenix would add unused downloads and competing toolchain ownership.

--- flake.nix
+++ flake.nix
@@ -3,11 +3,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
 
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     # Retained until the portable profile adopts the external Neovim package.
     vt-nvim.url = "git+https://codeberg.org/vtho/nvim";
     vt-nvim.inputs.nixpkgs.follows = "nixpkgs";
